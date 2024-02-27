e35f5213f20ac3bff8eafa4bcbf24d24ef7b0019
Samstiles-Dev <ayosamuel777@gmail.com>
2024-02-27 15:49:32 -0800

    amenities

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -32,7 +32,7 @@ def get_amenity(amenity_id):
     return jsonify(amenity.to_dict())
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['DELETE'],
+@app_views.route('/amenities/<amenity_id>/', methods=['DELETE'],
                  strict_slashes=False)
 @swag_from('documentation/amenity/delete_amenity.yml', methods=['DELETE'])
 def delete_amenity(amenity_id):
